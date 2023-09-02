02fd5dc6241aa1321d4ce2c1124a0a9562ade996
TRoYHD <dark_angle.2010@hotmail.com>
2023-09-02 14:46:03 +0300

    new update 1.1

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -38,7 +38,159 @@ class DoublyLinkedList {
     return removedNode.value;
   }
 
-  // Other methods (shift, unshift, get, set, insert, remove) can be implemented similarly
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    const removedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removedNode.next;
+      this.head.prev = null;
+      removedNode.next = null;
+    }
+    this.length--;
+    return removedNode.value;
+  }
+   // insert method
+   insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(val);
+
+    if (index === 0) {
+      // Insert at the beginning
+      newNode.next = this.head;
+      if (this.head) {
+        this.head.prev = newNode;
+      } else {
+        this.tail = newNode;
+      }
+      this.head = newNode;
+    } else if (index === this.length) {
+      // Insert at the end
+      newNode.prev = this.tail;
+      if (this.tail) {
+        this.tail.next = newNode;
+      } else {
+        this.head = newNode;
+      }
+      this.tail = newNode;
+    } else {
+      // Insert at the specified index
+      const currentNode = this.getNodeAt(index);
+      const previousNode = currentNode.prev;
+      newNode.next = currentNode;
+      newNode.prev = previousNode;
+      previousNode.next = newNode;
+      currentNode.prev = newNode;
+    }
+
+    this.length++;
+    return true;
+  }
+
+  // Helper method to get the node at a specific index
+  getNodeAt(index) {
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+    // get method
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        
+        if (index <= this.length / 2) {
+          // Search from the beginning
+          let current = this.head;
+          for (let i = 0; i < index; i++) {
+            current = current.next;
+          }
+          return current.value;
+        } else {
+          // Search from the end (to optimize for large indices)
+          let current = this.tail;
+          for (let i = this.length - 1; i > index; i--) {
+            current = current.prev;
+          }
+          return current.value;
+        }
+      }
+       // set method
+  set(index, val) {
+    if (index < 0 || index >= this.length) return false;
+
+    if (index <= this.length / 2) {
+      // Search from the beginning
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current.next;
+      }
+      current.value = val;
+    } else {
+      // Search from the end (to optimize for large indices)
+      let current = this.tail;
+      for (let i = this.length - 1; i > index; i--) {
+        current = current.prev;
+      }
+      current.value = val;
+    }
+    
+    return true;
+  }
+       // remove method
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+
+    if (index === 0) {
+      // Remove the head
+      if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+        this.head.prev = null;
+      }
+    } else if (index === this.length - 1) {
+      // Remove the tail
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    } else {
+      // Remove a node in the middle
+      const currentNode = this.getNodeAt(index);
+      const previousNode = currentNode.prev;
+      const nextNode = currentNode.next;
+      previousNode.next = nextNode;
+      nextNode.prev = previousNode;
+    }
+
+    this.length--;
+    return true;
+  }
+
+  // Helper method to get the node at a specific index
+  getNodeAt(index) {
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+    
 }
 
 module.exports = DoublyLinkedList;
